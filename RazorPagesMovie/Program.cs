44ad5265a6b68e7fb34c354b59d38021c3f42ff5
Rodney Woollett <rodney.woollett@gmail.com>
2022-02-19 08:32:00 +1300

    Initial commit

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+if (builder.Environment.IsDevelopment())
+{
+  builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext")));
+} else {
+  builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMovieContext")));
+}
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+  var services = scope.ServiceProvider;
+
+  SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+var supportedCultures = new[]
+{
+  new CultureInfo("en-NZ"),
+};
+app.UseRequestLocalization(new RequestLocalizationOptions{
+  DefaultRequestCulture = new RequestCulture("en-NZ"),
+  SupportedCultures=supportedCultures,
+  SupportedUICultures=supportedCultures
+});
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
